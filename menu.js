dce6d7824e3f627d54515aac218c32546f62c39e
trieuluong6 <trieuluong6@gmail.com>
2026-07-15 16:06:20 +0700

    Update menu categories and items

--- menu.js
+++ menu.js
@@ -1,69 +1,63 @@
-// menu.js
 export const menu = [
   {
-    cat: "MÓN ĐẶC BIỆT / ĐỒ TRỘN",
+    cat: "PHỞ",
     items: [
-      { id: 1, name: "Cơm sườn + trứng", price: 40000 },
-      { id: 2, name: "Cơm gà xé", price: 40000 },
-      { id: 3, name: "Cơm đùi gà chiên mắm", price: 40000 },
-      { id: 4, name: "Cơm chiên hải sản", price: 50000 },
-      { id: 5, name: "Cơm đùi gà xối mỡ", price: 50000 },
-      { id: 6, name: "Cơm bò xào rau củ", price: 60000 },
-      { id: 7, name: "Mỳ xào bò", price: 60000 },
-      { id: 8, name: "Mỳ xào hải sản", price: 60000 },
-      { id: 9, name: "Miến xào bò", price: 60000 },
-      { id: 10, name: "Miến xào hải sản", price: 60000 },
-      { id: 11, name: "Miến nước gà", price: 60000 },
-      { id: 12, name: "Miến nước bò", price: 60000 },
-      { id: 13, name: "Miến nước hải sản", price: 60000 }
+      { id: 1, name: "Phở Tái", price: 50000 },
+      { id: 2, name: "Phở Nạm", price: 50000 },
+      { id: 3, name: "Phở Bò Bằm", price: 50000 },
+      { id: 4, name: "Phở Bò Viên", price: 50000 },
+      { id: 5, name: "Phở Tái Nạm", price: 60000 },
+      { id: 6, name: "Phở Đặc Biệt", price: 70000 },
+      { id: 7, name: "Quẩy", price: 5000 },
+      { id: 8, name: "Trứng Trần", price: 10000 }
     ]
   },
+
   {
-    cat: "MÓN ĂN KÈM / ĐỒ HẤP",
+    cat: "MÌ",
     items: [
-      { id: 14, name: "Cháo nghêu", price: 40000 },
-      { id: 15, name: "Cháo bò", price: 40000 },
-      { id: 16, name: "Nghêu hấp", price: 50000 },
-      { id: 17, name: "Nghêu xào", price: 55000 },
-      { id: 18, name: "Tôm hấp", price: 100000 },
-      { id: 19, name: "Tôm chiên mắm", price: 105000 },
-      { id: 20, name: "Mực hấp", price: 150000 },
-      { id: 21, name: "Mực chiên mắm", price: 155000 }
+      { id: 9, name: "Mì Tôm Mực", price: 50000 },
+      { id: 10, name: "Mì Tôm Bò", price: 45000 },
+      { id: 11, name: "Mì Ý Bò Bằm", price: 79000 }
     ]
   },
+
+  {
+    cat: "BÁNH MÌ",
+    items: [
+      { id: 12, name: "Bánh Mì Xá Xíu", price: 30000 },
+      { id: 13, name: "Bánh Mì Xá Xíu Chả", price: 35000 },
+      { id: 14, name: "Bánh Mì Xá Xíu Trứng", price: 40000 },
+      { id: 15, name: "Bánh Mì Bò", price: 40000 },
+      { id: 16, name: "Bánh Mì Đặc Biệt", price: 50000 }
+    ]
+  },
+
+  {
+    cat: "CƠM",
+    items: [
+      { id: 17, name: "Cơm Chiên Bò", price: 69000 },
+      { id: 18, name: "Cơm Chiên Trái Thơm", price: 59000 },
+      { id: 19, name: "Cơm Chiên Hải Sản", price: 79000 },
+      { id: 20, name: "Cơm Chiên Rau Củ", price: 59000 }
+    ]
+  },
+
   {
     cat: "MÓN THÊM",
     items: [
-      { id: 22, name: "Trứng thêm", price: 10000 },
-      { id: 23, name: "Canh khổ qua thêm", price: 10000 },
-      { id: 24, name: "Ram chả giò (1 cái)", price: 10000 },
-      { id: 25, name: "Sườn nướng thêm", price: 25000 },
-      { id: 26, name: "Đùi gà chiên mắm thêm", price: 30000 },
-      { id: 27, name: "Phần gà xé thêm", price: 30000 },
-      { id: 40, name: "Cơm thêm", price: 5000 },
-      { id: 28, name: "Đùi gà xối mỡ thêm", price: 40000 },
-      { id: 41, name: "Bò xào rau củ thêm", price: 50000 },
+      { id: 21, name: "Bánh Phở Thêm", price: 5000 },
+      { id: 22, name: "Thịt Bò Thêm", price: 30000 }
     ]
   },
+
   {
-    cat: "NƯỚC UỐNG (DRINKING WATER)",
+    cat: "ĐỒ UỐNG",
     items: [
-      { id: 29, name: "Nước ngọt các loại", price: 15000 },
-      { id: 30, name: "Bò húc", price: 20000 },
-      { id: 31, name: "Nước suối", price: 10000 },
-      { id: 32, name: "Nước khoáng lạt", price: 10000 },
-      { id: 33, name: "Nước dừa trái", price: 30000 },
-      { id: 34, name: "Nước cam tươi", price: 20000 }
-    ]
-  },
-  {
-    cat: "BEER MENU",
-    items: [
-      { id: 35, name: "Larue", price: 18000 },
-      { id: 36, name: "Huda", price: 18000 },
-      { id: 37, name: "Saigon", price: 18000 },
-      { id: 38, name: "Tiger", price: 22000 },
-      { id: 39, name: "Heineken", price: 25000 }
+      { id: 23, name: "Nước Suối", price: 10000 },
+      { id: 24, name: "Bò Húc", price: 20000 },
+      { id: 25, name: "Revive", price: 20000 },
+      { id: 26, name: "Sting", price: 20000 }
     ]
   }
 ];
